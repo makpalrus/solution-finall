60ebaa551d07c6e24eeec149dd86b613fae7a0d2
Makpal <maqpal.rus@gmail.com>
2025-04-16 17:38:15 +0500

    base classes

---
+++ src/main/java/com/example/main/Game.java
@@ -0,0 +1,15 @@
+package com.example.main;
+
+public class Game {
+
+    private GameWindow gameWindow;
+    private GamePanel gamePanel;
+
+    public Game() {
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
+
+    }
+
+}
+
